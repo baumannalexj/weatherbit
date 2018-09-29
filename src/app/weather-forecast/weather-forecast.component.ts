4ed3f5c04c60fb14239bf4e52ed4bc21a10d739f
baumannalexj <baumannalexj@gmail.com>
2018-09-29 16:36:47 -0500

    abtracted into app-weather-forecast compoennt

--- src/app/weather-forecast/weather-forecast.component.ts
+++ src/app/weather-forecast/weather-forecast.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {weatherBit} from "../../environments/environment";
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-weather-forecast',
@@ -7,10 +9,37 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class WeatherForecastComponent implements OnInit {
 
-  @Input() weatherForecasts: any[];
+  weatherBitUrl: string;
+  weatherForecasts: any[];
+  cityDetails: any;
+  searchText: string;
 
-  constructor() {
-    console.log('weather forecast component')
+  constructor(private http: HttpClient) {
+    this.weatherBitUrl = `${weatherBit.config.host}/v2.0/forecast/daily?city=detroit&state=michigan&key=${weatherBit.config.apiKey}`;
+    this.weatherForecasts = [];
+    this.cityDetails = {
+      cityName: "",
+      stateCode: ""
+    };
+
+
+    console.log("api key: " + weatherBit.config.apiKey);
+  }
+
+  getWeather(){
+    this.weatherBitUrl = `${weatherBit.config.host}/v2.0/forecast/daily?city=${this.searchText}&key=${weatherBit.config.apiKey}`;
+
+    this.http
+      .get(this.weatherBitUrl)
+      .subscribe( (results: any)=>{
+
+        this.weatherForecasts = results['data'];
+        this.cityDetails.cityName = results['city_name'];
+        this.cityDetails.stateCode= results['state_code'];
+        console.log('results');
+        console.log(results);
+      });
+    //
   }
 
   ngOnInit() {
